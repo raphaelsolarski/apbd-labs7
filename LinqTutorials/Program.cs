5bb569ebf891b14febd64f80320255fb81e0a669
Rafał Solarski <solarski.rafal@gmail.com>
2024-06-29 21:31:31 +0200

    Initial version

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.Console.WriteLine("E 1");
+            System.Console.WriteLine(String.Join(", ", LinqTasks.Task1()));
+            
+            
+        }
+    }
+}
